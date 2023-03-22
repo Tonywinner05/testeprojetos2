49bf326af6be01d1236a63a0772e9805e6873de2
Tonywinner05 <128102030+Tonywinner05@users.noreply.github.com>
2023-03-21 23:32:21 -0300

    Create atividade1.c

---
+++ atividade1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char nome[] = "Antonio Maria Gomes Junior";
+    int numero = 202206840046;
+    printf("Aluno %\n", nome);
+    printf("Matricula %\n", numero);
+    return 0;
+}
