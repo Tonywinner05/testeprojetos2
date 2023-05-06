56a91281b3091ab03f502cd92ec21044015181c8
Antônio Junior <128102030+Tonywinner05@users.noreply.github.com>
2023-05-06 16:17:37 -0300

    Delete Nome-Matricula.c

--- Nome-Matricula.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-    int main()
-{
-        printf("Nome: Gabriela Melo Oliveira da Costa\n");
-        printf("Matricula: 202207040029\n");
-    return 0;
-}
