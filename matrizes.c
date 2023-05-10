4e2a272aa0527d30e62e9e309b512bf45c655185
Tonywinner05 <tony0101.txt@gmail.com>
2023-05-10 10:20:55 -0300

    criação da base das demais funções de matrizes

--- matrizes.c
+++ matrizes.c
@@ -4,10 +4,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <complex.h>
-#define M 3 
 #define N 3 
 //Matriz Transposta
+teste_trasposta(){
+	
+	void transpose(int mat[N][N]) {
+    int transposed[N][N];
+    int i, j;
 
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            transposed[i][j] = mat[j][i];
+        }
+    }
+
+    printf("Matriz transposta:\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            printf("%d ", transposed[i][j]);
+        }
+        printf("\n");
+    	}
+	}
+}
 // Matriz Conjugada
 
 teste_conjugada() {
@@ -54,6 +73,49 @@ teste_conjugada() {
     return 0;
 }
 // matriz hermitiana
+teste_hermitiana(){
+	
+}
+
+// soma de matrizes 
+teste_soma(){
+}
+
+// subtração de matrizes
+teste_subtração(){
+}
+
+// produto escalar
+teste_produto_escalar(){
+}
+// produto matricial
+teste_produto_matricial(){
+}
+// execução de todas as matrizes
+teste_todos(){
+}
+
+int main(){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	printf("Matriz original:\n");
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
 
 
 
