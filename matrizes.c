33f9727258eb2794be333dfbbc6f71c7ed0a3f95
Tonywinner05 <tony0101.txt@gmail.com>
2023-05-10 12:03:17 -0300

    ajuste de formas

--- matrizes.c
+++ matrizes.c
@@ -8,13 +8,13 @@
 //Matriz Transposta
 teste_trasposta(){
 	
-	void transpose(int mat[N][N]) {
+	void transpose(int mat1[N][N]) {
     int transposed[N][N];
     int i, j;
 
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
-            transposed[i][j] = mat[j][i];
+            transposed[i][j] = mat1[j][i];
         }
     }
 
@@ -29,42 +29,35 @@ teste_trasposta(){
 }
 // Matriz Conjugada
 
-teste_conjugada() {
-    complex float matriz[N][N];
-
+teste_conjugada(){
+	
+    void conjugade()
+	complex float matriz[N][N];
+ 	int i, j;
     // LÃª a matriz complexa do usuÃ¡rio
-    printf("Digite os elementos da matriz complexa (%dx%d):\n", N, N);
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            float real, imag;
-            printf("Real[%d][%d]: ", i+1, j+1);
-            scanf("%f", &real);
-            printf("Imag[%d][%d]: ", i+1, j+1);
-            scanf("%f", &imag);
-            matriz[i][j] = real + imag*I;
-        }
-    }
-
+    
+    
+    matriz[i][j] = real + imag*I;
     // Calcula a matriz conjugada
     complex float conjugada[N][N];
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for ( i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
             conjugada[i][j] = conj(matriz[i][j]);
         }
     }
 
     // Imprime as matrizes na tela
     printf("\nMatriz complexa:\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
             printf("%.1f + %.1fi\t", creal(matriz[i][j]), cimag(matriz[i][j]));
         }
         printf("\n");
     }
 
     printf("\nMatriz conjugada:\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
             printf("%.1f%.1fi\t", creal(conjugada[i][j]), cimag(conjugada[i][j]));
         }
         printf("\n");
@@ -82,34 +75,60 @@ teste_soma(){
 }
 
 // subtração de matrizes
-teste_subtração(){
+teste_sub(){
 }
-
 // produto escalar
 teste_produto_escalar(){
 }
 // produto matricial
 teste_produto_matricial(){
+
 }
 // execução de todas as matrizes
 teste_todos(){
+
 }
 
-int main(){
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+int main() {
+	int mat[N][N];
+    int i, j;
+    int escolha;
+    printf("Digite os elementos da matriz 3x3:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("Digite o elemento [%d][%d]: ", i, j);
+            scanf("%d", &mat[i][j]);
+            }
+        }
+        
+    printf("Escolha a manipulação matricial de 3x3 que deseja executar:\n");
+    printf("1. matriz trasposta \n");
+    printf("2. matriz conjugada \n");
+    printf("3. matriz hermitian \n");
+    printf("Escolha: ");
+    scanf("%d", &escolha);
+
+    switch (escolha) {
+        case 1:
+            teste_trasposta();
+            break;
+        case 2:
+            teste_conjugada();
+            break;
+        case 3:
+            teste_soma();
+            break;
+        default:
+            printf("Opção inválida!\n");
+            break;
+}
+
 	printf("Matriz original:\n");
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
-            printf("%d ", mat[i][j]);
+            printf("%d ",mat[i][j]);
         }
         printf("\n");
     }
