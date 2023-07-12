9b850b40fbf3d9e21e9567eccacb18d1680b9382
Antônio Junior <128102030+Tonywinner05@users.noreply.github.com>
2023-07-12 02:06:55 -0300

    configuração da man apos entrada da implemnetação do GSL

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+// Função de teste para a função calc_svd
+void teste_calc_svd();
+
+int main()
+{
+    printf("Equipe:\n");
+    printf("- Atônio MAria Gomes Junior\n");
+    printf("- Breno de Medeiros Paulo\n");
+    printf("- Gabriela Melo Oliveira da Costa\n");
+    printf("- Leonardo Da Silva Pinheiro \n");
+    printf("- Nome do Membro 5\n");
+
+    printf("\n");
+
+    printf("Teste da função calc_svd:\n");
+    teste_calc_svd();
+
+    return 0;
+}
